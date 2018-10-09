8d0d389c2b4ba70ef2fa99170c46d13f7056b1c2
ysengrimm <ysengrimm@grimmbart.org>
2018-10-09 17:40:14 +0200

    Create all files necessary for the plugin

---
+++ spec/atom-hets-integration-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomView from '../lib/atom-hets-integration-view';
+
+describe('AtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
